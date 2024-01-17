7a35b153609b2673ae72fb302fc548ca5ea2da4a
Marc Catalan Sanchez <mcatalan@student.42barcelona.com>
2024-01-17 10:50:27 +0100

    deleting out, finished parsing split with quotes prioritation

--- other/split.c
+++ other/split.c
@@ -14,62 +14,137 @@
 #include <stdlib.h>
 #include <string.h>
 
-char **ft_split_shell(const char *str)
+// char **ft_split_shell(const char *str)
+// {
+// 	int i;
+// 	int j;
+// 	int k;
+// 	int count;
+// 	char **arr;
+
+// 	i = 0;
+// 	j = 0;
+// 	count = 0;
+// 	arr = malloc(sizeof(char *) * 100);
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '\'' || str[i] == '\"')
+// 		{
+// 			i++;
+// 			while (str[i] && str[i] != '\'' && str[i] != '\"')
+// 				i++;
+// 			i++;
+// 		}
+// 		if (str[i] == '|')
+// 		{
+// 			arr[count] = malloc(sizeof(char) * (i - j + 1));
+// 			k = 0;
+// 			while (j < i)
+// 			{
+// 				arr[count][k] = str[j];
+// 				j++;
+// 				k++;
+// 			}
+// 			arr[count][k] = '\0';
+// 			count++;
+// 			j = i + 1;
+// 		}
+// 		i++;
+// 	}
+// 	arr[count] = malloc(sizeof(char) * (i - j + 1));
+// 	k = 0;
+// 	while (j < i)
+// 	{
+// 		arr[count][k] = str[j];
+// 		j++;
+// 		k++;
+// 	}
+// 	arr[count][k] = '\0';
+// 	count++;
+// 	arr[count] = NULL;
+// 	return (arr);
+// }
+
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	int i;
-	int j;
-	int k;
-	int count;
-	char **arr;
+	size_t	i;
 
 	i = 0;
-	j = 0;
-	count = 0;
-	arr = malloc(sizeof(char *) * 100);
-	while (str[i])
+	while (i < n && src[i] != '\0')
 	{
-		if (str[i] == '\'' || str[i] == '\"')
-		{
-			i++;
-			while (str[i] && str[i] != '\'' && str[i] != '\"')
-				i++;
-			i++;
-		}
-		if (str[i] == '|')
-		{
-			arr[count] = malloc(sizeof(char) * (i - j + 1));
-			k = 0;
-			while (j < i)
-			{
-				arr[count][k] = str[j];
-				j++;
-				k++;
-			}
-			arr[count][k] = '\0';
-			count++;
-			j = i + 1;
-		}
+		dest[i] = src[i];
 		i++;
 	}
-	arr[count] = malloc(sizeof(char) * (i - j + 1));
-	k = 0;
-	while (j < i)
+	while (i < n)
 	{
-		arr[count][k] = str[j];
-		j++;
-		k++;
+		dest[i] = '\0';
+		i++;
 	}
-	arr[count][k] = '\0';
-	count++;
-	arr[count] = NULL;
-	return (arr);
+	return (dest);
 }
 
-int main(int argc, char **argv)
+char	**ft_split_shell(const char *str)
 {
-	// const char *str = "This is a sentence | This is another sentence | \"This is a sentence| inside quotes\" | 'This is a sente|nce inside single quotes'";
-	const char *str = "hola | q \"klk|'";
-	char	*arr;
+	char	**result;
+	int commandcount = 0;
+	int start = 0;
+	int i = 0;
+	int	k = 0;
+	char flag = '\0';
+
+	while (str[i])
+	{
+		while (str[i] && str[i] == '|')
+			i++;
+		if (str[i] && str[i] != '|')
+			commandcount++;
+		while (str[i] && (str[i] != '|' || flag))
+		{
+			if (str[i] == '\'' && !flag)
+				flag = '\'';
+			else if (str[i] == '\"' && !flag)
+				flag = '\"';
+			else if (flag == str[i])
+				flag = '\0';
+			i++;
+		}
+	}
+	flag = '\0';
+	result = malloc(sizeof(char *) * (commandcount + 1));
+	i = 0;
+	while (str[i])
+	{
+		start = -1;
+		while (str[i] && str[i] == '|')
+			i++;
+		if (str[i] && str[i] != '|')
+			start = i;
+		while (str[i] && (str[i] != '|' || flag))
+		{
+			if (str[i] == '\'' && !flag)
+				flag = '\'';
+			else if (str[i] == '\"' && !flag)
+				flag = '\"';
+			else if (flag == str[i])
+				flag = '\0';
+			i++;
+		}
+		if (start < i && start != -1)
+		{
+			result[k] = malloc(sizeof(char) * (i - start + 1));
+			ft_strncpy(result[k], &str[start], (i - start));
+			k++;
+		}
+	}
+	result[k] = NULL;
+	return (result);
+}
+
+int main()
+{
+	const char *str = "This is a sentence | This is another sentence | \"This is a sentence| inside quotes\" | 'This is a sente|nce inside single quotes'";
+	// const char *str = "hola | q \"klk|'";
+	char	**arr;
 	arr = ft_split_shell(str);
 	int count = 0;
 	
@@ -79,7 +154,7 @@ int main(int argc, char **argv)
 		count++;
 	}
 	free(arr);
-	
+
 	return 0;
 }
 
